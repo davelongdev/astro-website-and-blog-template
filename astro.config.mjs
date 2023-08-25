d03e5a37b3b84c49395625f9eb0b9dc256efe911
dave long <davelongdev@gmail.com>
2023-08-25 17:55:33 -0400

    update astro.config.mjs with vercel url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-cip.netlify.app',
+  site: 'https://astro-blog-2.vercel.app',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), sitemap()]
