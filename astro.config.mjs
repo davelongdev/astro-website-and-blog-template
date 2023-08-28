d1ad93a1aebaf87b98bc1bf749d28fd20f3336f2
dave long <davelongdev@gmail.com>
2023-08-28 10:27:52 -0400

    add tailwind to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-2.vercel.app',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()]
+  }), sitemap(), tailwind()]
 });
