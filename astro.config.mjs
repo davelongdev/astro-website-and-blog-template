f2d1bd6649f7a532017803de34e2918c4750881e
dave long <davelongdev@gmail.com>
2024-01-07 19:08:03 -0500

    remove @astrojs/image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 import tailwind from "@astrojs/tailwind";
@@ -7,7 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-2.vercel.app',
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap(), tailwind()]
+  integrations: [sitemap(), tailwind()]
 });
